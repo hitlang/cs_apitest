0e3b7b6dd729f5f1fa8884cad298669034867d59
liulang <newlang@126.com>
2020-09-13 10:00:05 +0800

    修改

--- examples/testabc.py
+++ examples/testabc.py
@@ -1,2 +1,19 @@
-#-*-coding:utf-8 -*-
-#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+# !/usr/bin/python3
+import unittest
+
+import requests
+
+
+class TestUsers(unittest.TestCase):
+    url = "http://10.255.0.187:9000/users"
+
+    # 第一个用例
+    def test_users(self):
+        res = requests.get(url=self.url).json()
+
+        self.assertIn("吴云露", res, "用例失败 ")
+
+        pass
+
+    pass
