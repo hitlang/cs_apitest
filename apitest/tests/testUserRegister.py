6fea370765b505d0fd0a82be7e53510824a47768
liulang <newlang@126.com>
2020-09-17 15:50:54 +0800

    修改

---
+++ apitest/tests/testUserRegister.py
@@ -0,0 +1,20 @@
+# -*-coding:utf-8 -*-
+# !/usr/bin/python3
+# @Author:liulang
+
+import unittest
+
+from apitest.common.log import Log
+
+
+class TestUserRegister(unittest.TestCase):
+
+    def setUp(self) -> None:
+        Log.getLogger().info("start ")
+
+    def test_1(self):
+        self.assertEqual(1, 1)
+        pass
+
+    def tearDown(self) -> None:
+        Log.getLogger().info("end")
