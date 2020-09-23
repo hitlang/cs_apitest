bfdfb48b531cba450824079365adc179acf6a1a8
liulang <newlang@126.com>
2020-09-23 11:04:12 +0800

    修改

--- apitest/tests/testShopinfo.py
+++ apitest/tests/testShopinfo.py
@@ -1,49 +1,25 @@
 # -*-coding:utf-8 -*-
 # !/usr/bin/python3
 # @Author:liulang
-
-
 import unittest
 import requests
-
 from apitest.config.config import Config
-
-cf  = Config()
-
+cf = Config()
 class TestShopInfo(unittest.TestCase):
-
     url = "shopinfo"
-
     def setUp(self) -> None:
-        global  cf
+        global cf
         scheme = cf.getHttpConf("scheme")
         baseurl = cf.getHttpConf("baseurl")
         self.apikey = cf.getApikey()
-        self.url  = scheme + "://" + baseurl +  "/" + TestShopInfo.url
+        self.url = scheme + "://" + baseurl + "/" + TestShopInfo.url
 
-
-        # self.url = "http://localhost/dbshop/Jsonapi/shopinfo"
-        #
-        # self.apikey = "ded63ea2d7bfc17264b83931b9045014"
-
-
-
-    # @unittest.skip("")
-    def test_1(self):
-        '''
-        冒烟用例
-        :return:
-        '''
+    def test_shopinfo_smoke(self):
         params = {
-
             "apikey": self.apikey
         }
         res = requests.get(url=self.url, params=params).json()
-
         self.assertEqual("success", res['status'])
-
-
 
     def tearDown(self) -> None:
         print("test over")
-
