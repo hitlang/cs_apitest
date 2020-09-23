d44a9923d7c082c19809bca5c5d890e27e360ba5
liulang <newlang@126.com>
2020-09-23 11:03:44 +0800

    修改

--- apitest/tests/func_conn/testConn.py
+++ apitest/tests/func_conn/testConn.py
@@ -14,35 +14,26 @@ class TestConn(unittest.TestCase):
         scheme = localconfig.getHttpConf("scheme")
         baseurl = localconfig.getHttpConf("baseurl")
         self.url = scheme + '://' + baseurl
-
         self.apikey = localconfig.getApikey()
 
-        # self.url = "http://localhost/dbshop/Jsonapi"
-        #
-        # self.apikey = "ded63ea2d7bfc17264b83931b9045014"
+        # 测试用例2
 
-    # 测试用例1
-    def test_1(self):
-
+    def test_connection_error_apikey(self):
         params = {
-
-            "apikey" : self.apikey
-        }
-        res = requests.get(url=self.url , params=params).json()
-
-
-
-        self.assertEqual(res['status'] , "success", "接口连接失败")
-
-    # 测试用例2
-    def test_2(self):
-        params = {
-
             "apikey": "123"
         }
         res = requests.get(url=self.url, params=params).json()
-
         self.assertEqual(res['status'], "error")
+
+
+    # 测试用例1
+    def test_connection_smoke(self):
+        params = {
+            "apikey" : self.apikey
+        }
+        res = requests.get(url=self.url , params=params).json()
+        self.assertEqual(res['status'] , "success", "接口连接失败")
+
 
     def tearDown(self) -> None:
         pass
