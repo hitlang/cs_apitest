ef3bf63d46e6bf6b2ed3c67c97575992d1d40e84
liulang <newlang@126.com>
2020-10-02 03:43:45 +0800

    修改

--- apitest/tests/testAd2.py
+++ apitest/tests/testAd2.py
@@ -3,9 +3,8 @@
 # @Author:liulang
 import unittest
 import json
-import requests
-
-from apitest.common.log import Log
+from apitest.common.configHttp import ConfigHttp
+from apitest.common.log import MyLog
 
 '''
 调用api内置广告接口测试
@@ -17,45 +16,44 @@ class TestAd(unittest.TestCase):
     uri = "/specialAd"
 
     def setUp(self) -> None:
-        Log.getLogger().info("广告调用标记接口测试用例 开始执行")
-        scheme = global_config.getHttpConf("scheme")
-        baseurl = global_config.getHttpConf("baseurl")
-        self.url = scheme + '://' + baseurl + self.uri
-        self.apikey = global_config.getApikey()
+        # MyLog.get_log().info("广告调用标记接口测试用例 开始执行")
+        # scheme = global_config.getHttpConf("scheme")
+        # baseurl = global_config.getHttpConf("baseurl")
+        # self.url = scheme + '://' + baseurl + self.uri
+        # self.apikey = global_config.getApikey()
+        pass
 
     def test_Ad_1(self):
-        Log.getLogger().info("测试用例1.开始执行")
+        MyLog.get_log().info("测试用例1.开始执行")
         # 测试数据
         test_data = "efg"
         params = {
-            "ad_code": test_data,
-            "apikey": "ded63ea2d7bfc17264b83931b9045014"
-
+            "ad_code": test_data
         }
-        Log.getLogger().debug("任何的调试信息")
+        MyLog.get_log().debug("任何的调试信息")
 
-        res = requests.get(url=self.url, params=params).json()
+        # res = requests.get(url=self.url, params=params).json()
+        res = ConfigHttp(uri=self.uri,params=params).get().json()
         expected = "{\"status\": \"success\", \"msg\": \"信息调用成功！\",\"result\": null}"
         expected = json.loads(expected)
         self.assertDictEqual(expected, res)
-        Log.getLogger().info("测试用例1.执行结束")
+        MyLog.get_log().info("测试用例1.执行结束")
 
     def test_Ad_2(self):
         # 测试数据
         test_data = "example1"
         params = {
             "ad_code": test_data,
-            "apikey": "ded63ea2d7bfc17264b83931b9045014"
         }
 
-        res = requests.get(url=self.url, params=params).json()
+        res = ConfigHttp(uri=self.uri,params=params).get().json()
         result = res.get('result')
         self.assertIn("test", result['dbapi_ad_body'])
 
 
 
     def tearDown(self) -> None:
-        Log.getLogger().info("广告调用标记接口测试用例 结束执行")
+        MyLog.get_log().info("广告调用标记接口测试用例 结束执行")
 
 
 if __name__ == '__main__':
