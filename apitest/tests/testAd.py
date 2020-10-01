6679322e8cedf1222f2e199a46d7b800e6a17616
liulang <newlang@126.com>
2020-10-02 03:43:38 +0800

    修改

--- apitest/tests/testAd.py
+++ apitest/tests/testAd.py
@@ -1,36 +1,24 @@
 # -*-coding:utf-8 -*-
 # !/usr/bin/python3
 # @Author:liulang
-import unittest
-import json
-import requests
-import paramunittest
-
-from apitest.common.log import Log
-
 '''
 调用api内置广告接口测试
 '''
-from config import global_config
+import unittest
+import paramunittest
+from apitest.common.configHttp import ConfigHttp
+from apitest.common.log import MyLog
+
+
 
 
 @paramunittest.parametrized(
     {"pramsname": "ad_code", "testdata": ""},
-
     {"pramsname": "ad_error", "testdata": ""},
-
     {"pramsname": "", "testdata": ""},
-
 )
 class TestAd(unittest.TestCase):
     uri = "/specialAd"
-
-    def setUp(self) -> None:
-        Log.getLogger().info("广告调用标记接口测试用例 开始执行")
-        scheme = global_config.getHttpConf("scheme")
-        baseurl = global_config.getHttpConf("baseurl")
-        self.url = scheme + '://' + baseurl + self.uri
-        self.apikey = global_config.getApikey()
 
     def setParameters(self, pramsname, testdata):
         '''
@@ -42,21 +30,25 @@ class TestAd(unittest.TestCase):
         self.testdata = testdata
 
     def test_345(self):
+
+
         '''
         参数化
         :return:
         '''
         params = {
-             self.pramsname: self.testdata,
-            "apikey": "ded63ea2d7bfc17264b83931b9045014"
+            self.pramsname: self.testdata
         }
-        print(params)
-        res = requests.get(url=self.url, params=params).json()
+
+        res = ConfigHttp(uri=self.uri, params=params).get().json()
+
+        # Log().getLogger().debug(res)
+        MyLog.get_log().debug(res) #修正
         self.assertFalse(res['result'])
         pass
 
     def tearDown(self) -> None:
-        Log.getLogger().info("广告调用标记接口测试用例 结束执行")
+        MyLog.get_log().info("广告调用标记接口测试用例 结束执行")
 
 
 if __name__ == '__main__':
