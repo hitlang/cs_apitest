d7fcdaba64a292593b1cf9be3f23e6c90a08ba14
liulang <newlang@126.com>
2020-10-02 03:43:53 +0800

    修改

--- apitest/tests/testShopinfo.py
+++ apitest/tests/testShopinfo.py
@@ -3,20 +3,22 @@
 # @Author:liulang
 import unittest
 import requests
+from apitest.common.configHttp import ConfigHttp
 from config import global_config
 class TestShopInfo(unittest.TestCase):
-    uri = "shopinfo"
+    uri = "/shopinfo"
     def setUp(self) -> None:
-        scheme = global_config.getHttpConf("scheme")
-        baseurl = global_config.getHttpConf("baseurl")
-        self.apikey = global_config.getApikey()
-        self.url = scheme + "://" + baseurl + "/" + self.uri
+        # scheme = global_config.getHttpConf("scheme")
+        # baseurl = global_config.getHttpConf("baseurl")
+        # self.apikey = global_config.getApikey()
+        # self.url = scheme + "://" + baseurl + "/" + self.uri
+
+        pass
 
     def test_shopinfo_smoke(self):
-        params = {
-            "apikey": self.apikey
-        }
-        res = requests.get(url=self.url, params=params).json()
+
+        # res = requests.get(url=self.url, params=params).json()
+        res = ConfigHttp(uri=self.uri).get().json()
         self.assertEqual("success", res['status'])
 
     def tearDown(self) -> None:
