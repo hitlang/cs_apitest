2eb100aa36618bc33ce35c501818af098c9bc400
liulang <newlang@126.com>
2020-10-05 00:49:44 +0800

    改写excel_utils

--- apitest/utils/excel_uitls.py
+++ apitest/utils/excel_uitls.py
@@ -2,10 +2,16 @@
 # @Author  : lang
 import os
 import openpyxl
+#引入列号转字母列号
 from openpyxl.utils.cell import get_column_letter
 import pprint
 class ExcelUtil:
     def __init__(self, sheetName, *, start_col=1, end_col=None) -> None:
+        '''
+        :param sheetName: sheet名
+        :param start_col: 切片开始列 从1开始
+        :param end_col: 切片结束列
+        '''
         wb = openpyxl.load_workbook(
             os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "testfiles", "test_cases.xlsx")))
         self.sheet = wb[sheetName]
@@ -13,7 +19,7 @@ class ExcelUtil:
         self.end_col_letter = get_column_letter(end_col)
 
     def get_dict(self):
-        start = "{}{}".format(self.start_col_letter, 1)
+        start = "{}{}".format(self.start_col_letter, 1) #包含第一行标题行
         end = "{}{}".format(self.end_col_letter, self.sheet.max_row)
         CELL_SLICE = slice(start, end)
         cells = self.sheet[CELL_SLICE]
@@ -24,11 +30,10 @@ class ExcelUtil:
             data_dict = {}
             for k, v in m:
                 data_dict[k] = v.value
-                pass
             ret.append(data_dict)
         return ret
 
-
+#for test
 if __name__ == '__main__':
     excel = ExcelUtil("login", start_col=1, end_col=8)
     r = excel.get_dict()
