56d09ed980b736fb3465366801f951d23cac02b5
liulang <newlang@126.com>
2020-10-05 13:11:01 +0800

    修改

---
+++ apitest/tests/testGoodsInfo.py
@@ -0,0 +1,23 @@
+#-*-coding:utf-8 -*-
+#!/usr/bin/python3
+# @Author:liulang
+import  unittest
+
+class TestGoodsInfo(unittest.TestCase):
+
+
+    def setUp(self) -> None:
+        super().setUp()
+
+
+    def test_1(self):
+
+        pass
+
+    def test_2(self):
+
+        pass
+
+    def tearDown(self) -> None:
+        super().tearDown()
+
