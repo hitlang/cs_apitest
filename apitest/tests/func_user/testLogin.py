33c5c43ff108656fc69e51fde36d986d593e7cd5
liulang <newlang@126.com>
2020-10-05 12:45:53 +0800

    改写excel_utils

--- apitest/tests/func_user/testLogin.py
+++ apitest/tests/func_user/testLogin.py
@@ -2,24 +2,16 @@
 # !/usr/bin/python3
 # @Author:liulang
 import unittest
-import requests
+from apitest.common.configHttp import ConfigHttp
+from apitest.common.log import MyLog
 
 
-# 声明全局变量
-from config import Config
-
-localconfig = Config()
-
-@unittest.skip("")
 class TestLogin(unittest.TestCase):
-    url = "login"
 
     def setUp(self) -> None:
-        global localconfig
-        scheme = localconfig.getHttpConf("scheme")
-        baseurl = localconfig.getHttpConf("baseurl")
-        self.url = scheme + '://' + baseurl + '/' + TestLogin.url
-
+        MyLog.get_log().info("----------------登录测试开始-------------------------")
+        self.configHttp = ConfigHttp(uri="/login")
+        pass
 
     def testLogin_1(self):
         '''
@@ -30,17 +22,11 @@ class TestLogin(unittest.TestCase):
             "user_name": "test1",
             "user_password": "123456"
         }
-
-        apikey = localconfig.getApikey()
-        params = {
-
-            "apikey": apikey
-        }
-        res = requests.post(url=self.url, data=payload, params=params).json()
-
+        self.configHttp.data = payload
+        res = self.configHttp.post().json()
         self.assertEqual("success", res['status'], "登录测试没有通过")
 
-    # @unittest.skip('')
+
     def testLogin_2(self):
         '''
         用户名错
@@ -50,13 +36,8 @@ class TestLogin(unittest.TestCase):
             "user_name": "test",
             "user_password": "123456"
         }
-
-        params = {
-
-            "apikey": localconfig.getApikey()
-        }
-        res = requests.post(url=self.url, data=payload, params=params).json()
-
+        self.configHttp.data = payload
+        res = self.configHttp.post().json()
         self.assertEqual("error", res['status'], "登录测试没有通过")
 
     def testLogin_3(self):
@@ -68,15 +49,10 @@ class TestLogin(unittest.TestCase):
             "user_name": "test",
             "user_password": "12345"
         }
-
-        params = {
-
-            "apikey": localconfig.getApikey()
-        }
-        res = requests.post(url=self.url, data=payload, params=params).json()
-
+        self.configHttp.data = payload
+        res = self.configHttp.post().json()
         self.assertEqual("error", res['status'], "登录测试没有通过")
 
-    # 只要setup成功执行，那么必定执行
+
     def tearDown(self) -> None:
-        print("test over")
+        MyLog.get_log().info("----------------登录测试结束-------------------------")
