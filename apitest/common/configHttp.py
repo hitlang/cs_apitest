2b1b99fd72f6e018dc90ac20b7aa5c2d77a98b30
liulang <newlang@126.com>
2020-10-07 19:26:09 +0800

    修改

--- apitest/common/configHttp.py
+++ apitest/common/configHttp.py
@@ -9,65 +9,72 @@ config = global_config
 
 
 class ConfigHttp():
-    def __init__(self, *, uri) -> None:
-        MyLog.get_log().debug("---------创建ConfigHttp对象---------")
-        scheme = config.getHttpConf("scheme")
-        baseurl = config.getHttpConf("baseurl")
+    def __init__(self, *, method, uri, params=None, data=None, headers=None) -> None:
+        self.scheme = config.getHttpConf("scheme")
+        self.baseurl = config.getHttpConf("baseurl")
         self.timeout = config.getHttpConf("timeout")
-        apikey = config.getApikey()
+        self.apikey = config.getApikey()
         self.url = "{scheme}://{baseurl}{uri}{apikey}" \
-            .format(scheme=scheme, baseurl=baseurl, uri=uri, apikey="?apikey=" + apikey if apikey else "")
-        self._params = {}
-        self._headers = {}
-        self._data = {}
+                     .format(scheme=self.scheme,
+                             baseurl= self.baseurl,
+                             uri=uri,
+                             apikey="?apikey=" + self.apikey if self.apikey else "")
+        self.params = params
+        self.headers = headers
+        self.data = data
+        self.method = method
 
-    @property
-    def params(self):
-        return self._params
+    def request(self):
 
-    @params.setter
-    def params(self, value):
-        self._params = value
+        if self.method.upper() == 'GET':
+            '''
+                   发送get 请求
+                   :return: 响应对象
+                   '''
+            try:
+                response = requests.get(url=self.url, params=self.params, headers=self.headers,
+                                        timeout=float(self.timeout))
+                return response
+            except TimeoutError:
+                MyLog.get_log().error("timeout")
+                pass
+        elif self.method.upper() == 'POST':
+            '''
+                    发送post请求
+                    :return:
+                    '''
+            try:
+                response = requests.post(self.url, data=self.data, headers=self.headers, timeout=float(self.timeout))
+                return response
+            except TimeoutError:
+                MyLog.get_log().error("timeout")
+                pass
 
-    @property
-    def headers(self):
-        return self._headers
+        return response
 
-    @headers.setter
-    def headers(self, value):
-        self._headers = value
-
-    @property
-    def data(self):
-        return self._data
-
-    @data.setter
-    def data(self, value):
-        self._data = value
-
-    def get(self):
-        '''
-        发送get 请求
-        :return: 响应对象
-        '''
-        try:
-            response = requests.get(url=self.url, params=self.params, headers=self.headers, timeout=float(self.timeout))
-            return response
-        except TimeoutError:
-            MyLog.get_log().error("timeout")
-            pass
-
-    def post(self):
-        '''
-        发送post请求
-        :return:
-        '''
-        try:
-            response = requests.post(self.url, data=self.data, headers=self.headers, timeout=float(self.timeout))
-            return response
-        except TimeoutError:
-            MyLog.get_log().error("timeout")
-            pass
+    # def get(self):
+    #     '''
+    #     发送get 请求
+    #     :return: 响应对象
+    #     '''
+    #     try:
+    #         response = requests.get(url=self.url, params=self.params, headers=self.headers, timeout=float(self.timeout))
+    #         return response
+    #     except TimeoutError:
+    #         MyLog.get_log().error("timeout")
+    #         pass
+    #
+    # def post(self):
+    #     '''
+    #     发送post请求
+    #     :return:
+    #     '''
+    #     try:
+    #         response = requests.post(self.url, data=self.data, headers=self.headers, timeout=float(self.timeout))
+    #         return response
+    #     except TimeoutError:
+    #         MyLog.get_log().error("timeout")
+    #         pass
 
     # def postWithJson(self):
     #     '''
