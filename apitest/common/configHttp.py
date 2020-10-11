cf0d8b144d10e3c59f4a1976c86803b740be9854
liulang <newlang@126.com>
2020-10-11 11:24:26 +0800

    修改

--- apitest/common/configHttp.py
+++ apitest/common/configHttp.py
@@ -15,10 +15,10 @@ class ConfigHttp():
         self.timeout = config.getHttpConf("timeout")
         self.apikey = config.getApikey()
         self.url = "{scheme}://{baseurl}{uri}{apikey}" \
-                     .format(scheme=self.scheme,
-                             baseurl= self.baseurl,
-                             uri=uri,
-                             apikey="?apikey=" + self.apikey if self.apikey else "")
+            .format(scheme=self.scheme,
+                    baseurl=self.baseurl,
+                    uri=uri,
+                    apikey="?apikey=" + self.apikey if self.apikey else "")
         self.params = params
         self.headers = headers
         self.data = data
@@ -50,56 +50,15 @@ class ConfigHttp():
                 MyLog.get_log().error("timeout")
                 pass
 
+        elif self.method.upper() == 'POSTWITHJSON':
+            try:
+                response = requests.post(self.url, headers=self.headers, json=self.data, timeout=float(self.timeout))
+                return response
+            except TimeoutError:
+                MyLog.get_log().error("Time out!")
+                pass
+
         return response
-
-    # def get(self):
-    #     '''
-    #     发送get 请求
-    #     :return: 响应对象
-    #     '''
-    #     try:
-    #         response = requests.get(url=self.url, params=self.params, headers=self.headers, timeout=float(self.timeout))
-    #         return response
-    #     except TimeoutError:
-    #         MyLog.get_log().error("timeout")
-    #         pass
-    #
-    # def post(self):
-    #     '''
-    #     发送post请求
-    #     :return:
-    #     '''
-    #     try:
-    #         response = requests.post(self.url, data=self.data, headers=self.headers, timeout=float(self.timeout))
-    #         return response
-    #     except TimeoutError:
-    #         MyLog.get_log().error("timeout")
-    #         pass
-
-    # def postWithJson(self):
-    #     '''
-    #     发送json数据
-    #     :return:
-    #     '''
-    #     try:
-    #         response = requests.post(self.url, headers=self.headers, json=self.data, timeout=float(self.timeout))
-    #         return response
-    #     except TimeoutError:
-    #         MyLog.get_log().error("Time out!")
-    #         pass
-
-    # def postWithFile(self):
-    #     fp = open(self.file_path, 'rb')
-    #     self.files = {"filename": fp}
-    #     try:
-    #         response = requests.post(self.url, headers=self.headers, data=self.data, files=self.files,
-    #                                  timeout=float(timeout))
-    #         fp.close()
-    #         return response
-    #     except TimeoutError:
-    #         self.logger.error("Time out!")
-    #         return None
-    #
 
 
 if __name__ == '__main__':
