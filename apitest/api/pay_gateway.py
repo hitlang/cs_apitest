b444c513befa90b382e37d1c7645587c048b7eb3
liulang <newlang@126.com>
2020-10-18 17:11:15 +0800

    修改

---
+++ apitest/api/pay_gateway.py
@@ -0,0 +1,12 @@
+#-*-coding:utf-8 -*-
+#!/usr/bin/python3
+# @Author:liulang
+
+
+def  get_orderPayFinish(configHttp):
+    '''
+    发送订单完成支付请求
+    :return:
+    '''
+    res = configHttp.request().json()
+    return res
