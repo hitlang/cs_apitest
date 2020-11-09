b1a5cfa589fede0455d7255df02878476a682b64
liulang <newlang@126.com>
2020-11-09 10:08:34 +0800

    修改

--- apitest/tests/appx/test_classification_add.py
+++ apitest/tests/appx/test_classification_add.py
@@ -1,16 +1,21 @@
-#-*-coding:utf-8 -*-
-#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+# !/usr/bin/python3
 # @Author:liulang
 import unittest
-import  time
+import time
 from apitest.common.configHttp import ConfigHttp
 from apitest.common.log import MyLog
 from apitest.params.params import AppxClassAdd
 from config import global_config
+import os
+
+test_pic_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             os.path.pardir,
+                                             os.path.pardir,
+                                             "data", "test_03.jpg"))
 
 
 class TestClassificationAdd(unittest.TestCase):
-
 
     def setUp(self) -> None:
         self.aca = AppxClassAdd()
@@ -23,38 +28,35 @@ class TestClassificationAdd(unittest.TestCase):
         上传分组图片
         :return:
         '''
-        image = open(r"E:\cs_apitest\cs_apitest\apitest\data\test_03.jpg", "rb")
-        url , data , headers , method  = self.aca.urls[0],self.aca.datas[0],self.aca.headers[0],self.aca.methods[0]
+        with  open(test_pic_path, "rb") as  fp:
+            url, data, headers, method = self.aca.urls[0], self.aca.datas[0], self.aca.headers[0], self.aca.methods[0]
 
-        headers.update({
-            "Authorization": self.config.getToken()
+            headers.update({
+                "Authorization": self.config.getToken()
 
-        })
-        files = {"file":image}
-        #when
-        res = ConfigHttp(method=method, url=url,  data=data, headers=headers, files=files).request().json()
+            })
+            files = {"file": fp}
+            # when
+            res = ConfigHttp(method=method, url=url, data=data, headers=headers, files=files).request().json()
 
-        self.log.debug("upload_file  res ==={}".format(res))
+            self.log.debug("upload_file  res ==={}".format(res))
 
-        #then
+            # then
 
-        self.assertEqual(res["code"] , "000000")
-        self.assertEqual(res["comment"] , "Completed successfully")
-        #out
+            self.assertEqual(res["code"], "000000")
+            self.assertEqual(res["comment"], "Completed successfully")
+            # out
 
-        image.close()
-
-        global  img_url
+        global img_url
 
         img_url = res["data"][0]
 
     def test_class_add_smoke(self):
-        global  img_url
+        global img_url
         '''
          增加分组
-        :return: 
+        :return:
         '''
-
 
         url, data, headers, method = self.aca.urls[1], self.aca.datas[1], self.aca.headers[1], self.aca.methods[1]
 
@@ -68,15 +70,12 @@ class TestClassificationAdd(unittest.TestCase):
             "logo": img_url
         })
 
-        res = ConfigHttp(method=method, url=url,  data=data, headers=headers).request().json()
+        res = ConfigHttp(method=method, url=url, data=data, headers=headers).request().json()
 
         self.log.debug("class add res ==={}".format(res))
 
-        #then
+        # then
 
-        self.assertEqual(res["code"] , "000000")
-        self.assertEqual(res["comment"] , "Completed successfully")
+        self.assertEqual(res["code"], "000000")
+        self.assertEqual(res["comment"], "Completed successfully")
         pass
-
-
-
